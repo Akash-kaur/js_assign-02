e9fdc0b853283639828fa1f4d5ee426454bfaba8
akashdeep <akash91deep@gmail.com>
2017-01-31 03:18:01 +0000

    first commit

---
+++ assign2.js
@@ -0,0 +1,13 @@
+let arr=['1','9','6'];
+let i=2;
+function arrArray(array,index){
+    console.log(array);
+ for(let i=0; i<=index; i++){
+     
+    
+     console.log("index values:" +i);
+     console.log("array values:"+ array[i]);
+     
+ }   
+}
+arrArray(arr,i);
